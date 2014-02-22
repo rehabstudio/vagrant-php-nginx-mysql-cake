e0915109fdbffab1ce6defaa6ee743bd546bb42d
Peter McConnell <pemcconnell@googlemail.com>
2014-02-22 11:27:00 +0000

    - added defined wrappers for package installs to reduce conflict issues

--- vagrant/puppet/manifests/apt_packages.pp
+++ vagrant/puppet/manifests/apt_packages.pp
@@ -2,21 +2,49 @@
 class { 'apt': }
 apt::ppa { 'ppa:git-core/ppa': }
 
-# Installing other useful packages.
-package { [
-    # latest stable git
-    'git-core',
-    # vim 'cos vim
-    'vim',
-    # augeas support
-    'libaugeas-ruby',
-    'augeas-tools',
-    # in-memory cache
-    'memcached',
-    # because we need to uncompress things
-    'unzip',
-    'zip'
-]:
-    ensure => present,
-    require => Apt::Ppa['ppa:git-core/ppa']
+# latest stable git
+if ! defined(Package['git']) { 
+    package { 'git':
+        ensure => installed, 
+        name => 'git-core',
+        require => Apt::Ppa['ppa:git-core/ppa']
+    }
+}
+
+# vim editor
+if ! defined(Package['vim']) { 
+    package { 'vim':
+        ensure => installed
+    }
+}
+
+# augeas support
+if ! defined(Package['libaugeas-ruby']) { 
+    package { 'libaugeas-ruby':
+        ensure => installed
+    }
+}
+if ! defined(Package['augeas-tools']) { 
+    package { 'augeas-tools':
+        ensure => installed
+    }
+}
+
+# in-memory cache
+if ! defined(Package['memcached']) { 
+    package { 'memcached':
+        ensure => installed
+    }
+}
+
+# compression support
+if ! defined(Package['unzip']) { 
+    package { 'unzip':
+        ensure => installed
+    }
+}
+if ! defined(Package['zip']) { 
+    package { 'zip':
+        ensure => installed
+    }
 }
