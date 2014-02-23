9c6829a99f774a2f84c487ce910b0bcbd0dee947
Peter McConnell <pemcconnell@googlemail.com>
2014-02-23 14:51:15 +0000

    - added composer

--- vagrant/puppet/manifests/php.pp
+++ vagrant/puppet/manifests/php.pp
@@ -1,37 +1,42 @@
-# Including necessary classes for installing PHP.
 include php
+include php::apt
+include php::composer
 
-# Installing FPM and PEAR separately as they have extra functionality.
 class {
-    'php::fpm':
-        ensure => installed;
-    'php::pear':
-        ensure => installed;
+  'php::cli':
+    ensure => present;
+  'php::dev':
+    ensure => present;
+  'php::pear':
+    ensure => present;
+  'php::extension::apc':
+    ensure => present;
+  'php::fpm':
+    ensure => present;
 }
 
-# Installing PHP Extensions.
+# installing PHP Extensions.
 php::extension { 'php-extensions':
     ensure => installed,
     package => [
-        'php5-curl', 'php5-dev',
-        'php5-gd', 'php5-imagick', 'php5-mcrypt',
+        'php5-curl', 'php5-gd', 'php5-imagick', 'php5-mcrypt',
         'php5-mysql', 'php5-pspell', 'php5-xdebug',
         'php5-xmlrpc', 'php5-tidy', 'php5-xsl'
     ],
     notify => Service['php5-fpm'];
 }
 
-# Installing PHPUnit via Pear.
-package { 'pear.phpunit.de/PHPUnit':
-    ensure   => present,
-    provider => pear,
-    require  => [Package['php-pear'], Exec['php::pear::auto_discover']];
-}
-
-# Updating particular settings in the ini file without doing a file overwrite.
+# updating particular settings in the ini file without doing a file overwrite.
 augeas { 'php.ini':
     notify => Service['php5-fpm'],
     require => [Package['libaugeas-ruby'], Package['augeas-tools'], Package['php5-fpm']],
     context => '/files/etc/php5/fpm/php.ini',
     changes => $settings::ymlconfig['php']['iniOverrides']
+}
+
+# phpunit
+package { 'pear.phpunit.de/PHPUnit':
+    ensure   => '3.7.12',
+    provider => pear,
+    require  => Exec['php::pear::auto_discover'];
 }
