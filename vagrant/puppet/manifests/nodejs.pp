0c45a251e6aecbf1afef61733f174d7661fead2e
Peter McConnell <pemcconnell@googlemail.com>
2014-02-23 12:41:48 +0000

    - made paths dynamic

--- vagrant/puppet/manifests/nodejs.pp
+++ vagrant/puppet/manifests/nodejs.pp
@@ -3,22 +3,20 @@ class { 'nodejs':
     manage_repo => true
 }
 
-
 # Installing node executables.
 package { [
     'grunt-cli',
     'bower'
 ]:
-    ensure => present,
+    ensure   => present,
     provider => 'npm',
-    require => Class['nodejs']
+    require  => Class['nodejs']
 }
-
 
 # Installing node dependencies.
 exec { 'Installing Node Packages':
-    cwd => "/home/vagrant/app",
+    cwd     => $settings::ymlconfig['env']['docRoot'],
     command => 'npm install',
     require => Class['nodejs'],
-    onlyif => "test -f /home/vagrant/app/package.json"
+    onlyif  => "test -f ${settings::ymlconfig[env][docRoot]}/package.json"
 }
