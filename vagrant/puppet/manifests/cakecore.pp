deda9878184778a49b424490dd1fbf85c3b8a416
Peter McConnell <pemcconnell@googlemail.com>
2014-02-23 12:43:20 +0000

    - syntax updates

--- vagrant/puppet/manifests/cakecore.pp
+++ vagrant/puppet/manifests/cakecore.pp
@@ -1,9 +1,9 @@
 # install cakephp core files
 class { 'cakeCore':
-    appRoot    => "${settings::ymlconfig[env][docRoot]}",
-    cakeRoot   => "${settings::ymlconfig[cake][corePath]}",
-    cakeTag    => "${settings::ymlconfig[cake][repoTag]}",
-    db_user    => "${settings::ymlconfig[mysql][user]}",
-    db_pass    => "${settings::ymlconfig[mysql][pass]}",
-    db_name    => "${settings::ymlconfig[mysql][name]}";
+    appRoot  => "${settings::ymlconfig[env][docRoot]}",
+    cakeRoot => "${settings::ymlconfig[cake][corePath]}",
+    cakeTag  => "${settings::ymlconfig[cake][repoTag]}",
+    db_user  => "${settings::ymlconfig[mysql][user]}",
+    db_pass  => "${settings::ymlconfig[mysql][pass]}",
+    db_name  => "${settings::ymlconfig[mysql][name]}";
 }
